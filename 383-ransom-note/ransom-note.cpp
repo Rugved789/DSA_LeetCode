19011191e242aed32b3200eb01e0e88f44ad5a32
Rugved gadge <164230794+Rugved789@users.noreply.github.com>
2026-07-15 22:52:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (11.72%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> need;
+        unordered_map<char,int> have;
+
+        for(int i=0;i<ransomNote.length();i++){
+            need[ransomNote[i]]++;
+        }
+        for(int i=0;i<magazine.length();i++){
+            have[magazine[i]]++;
+        }
+
+        for(auto i : need){
+            char c = i.first;
+            int needval = i.second; 
+            int haveval = have[c];
+            if(needval>haveval){
+                return false;
+            }
+        }
+        return true;
+    }
+};
