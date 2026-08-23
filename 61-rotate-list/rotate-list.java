e7541fca5a8f5121589eff1d60c0fad049dffd04
Rugved gadge <164230794+Rugved789@users.noreply.github.com>
2026-08-23 18:17:28 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (10.13%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -11,7 +11,7 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         ListNode tail = head;
-        int len = 1;
+        int len=1;
         if(head==null || head.next==null || k==0){return head;}
         while(tail.next!=null){
             tail=tail.next;
@@ -19,12 +19,12 @@ class Solution {
         }
         tail.next=head;
         k=k%len;
-        int lastnode = len-k;
-        ListNode newTail = head;
-        for(int i=1;i<lastnode;i++){
+        int lastNode = len-k;
+        ListNode newTail=head;
+        for(int i=1;i<lastNode;i++){
             newTail=newTail.next;
         }
-        ListNode newHead=newTail.next;
+        ListNode newHead = newTail.next;
         newTail.next=null;
         return newHead;
     }
