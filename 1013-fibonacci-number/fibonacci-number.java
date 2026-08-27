e1fea50a3c8ed894f3fe6f1a4c0906f5a9b1229f
Rugved gadge <164230794+Rugved789@users.noreply.github.com>
2026-08-27 20:44:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (33.38%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -2,18 +2,12 @@ class Solution {
     public int fib(int n) {
         int a=0;
         int b=1;
-        int c=0;
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        for(int i=1;i<n;i++){
-            c=a+b;
+
+        for(int i=0;i<n;i++){
+            int c=a+b;
             a=b;
             b=c;
         }
-        return c;
+        return a;
     }
 }
