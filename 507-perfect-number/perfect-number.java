af3eaacd1093cdb2f0d1f610a3c855361a89b400
Rugved gadge <164230794+Rugved789@users.noreply.github.com>
2026-09-08 01:52:35 +0530

    Time: 2094 ms (41.24%) | Memory: 42.2 MB (25.58%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int result = 0;
+        for(int i=1;i<=num/2;i++){
+            if(num%i==0){
+                result = result + i;
+            }
+        }
+        if(result==num){
+            return true;
+        }
+        return false;
+    }
+}
